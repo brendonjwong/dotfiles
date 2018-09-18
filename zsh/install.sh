a07ce3d632d4f4458f9fab58b61b348acb7e1e62
brendonjwong <bjwong.w@gmail.com>
2018-09-17 18:47:37 -0700

    Install powerline fonts

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,3 +14,10 @@ if [ -f ~/.zshrc.pre-oh-my-zsh ] || [ -h ~/.zshrc.pre-oh-my-zsh ]; then
   printf "Found ~/.zshrc.pre-oh-my-zsh. Renaming to .zshrc\n"
   mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 fi
+
+# Install Powerline fonts for ZSH themes
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts

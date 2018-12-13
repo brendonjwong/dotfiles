69dfff2969ce0fadf191bfa09ee978caed724512
brendonjwong <bjwong.w@gmail.com>
2018-12-12 18:50:35 -0800

    Update lein profiles for personal computer.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[amperity/lein-cljfmt "0.7.0-SNAPSHOT"]
-                  [jonase/eastwood "0.3.3"]
+{:user {:plugins [[jonase/eastwood "0.3.3"]
                   [lein-monolith "1.1.0"]
                   [lein-pprint "1.2.0"]
+                  [mvxcvi/lein-cljfmt "0.7.0-SNAPSHOT"]
                   [mvxcvi/whidbey "1.3.2"]]
         :dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
@@ -9,7 +9,7 @@
 
  :dev {:dependencies [[reloaded.repl "0.2.4"]]}
 
- :my/repl ^:repl {:dependencies [[cider/cider-nrepl "0.17.0"]
+ :my/repl ^:repl {:dependencies [[cider/cider-nrepl "0.18.0"]
                                  [refactor-nrepl "2.4.0"]]
                   :repl-options {:nrepl-middleware
                                  [cider.nrepl/wrap-apropos
@@ -22,6 +22,9 @@
                                   cider.nrepl/wrap-macroexpand
                                   cider.nrepl/wrap-ns
                                   cider.nrepl/wrap-spec
+                                  cider.nrepl/wrap-pprint
+                                  cider.nrepl/wrap-pprint-fn
+                                  cider.nrepl/wrap-profile
                                   cider.nrepl/wrap-refresh
                                   cider.nrepl/wrap-resource
                                   cider.nrepl/wrap-stacktrace
@@ -29,5 +32,4 @@
                                   cider.nrepl/wrap-trace
                                   cider.nrepl/wrap-out
                                   cider.nrepl/wrap-undef
-                                  cider.nrepl/wrap-version]}
-                  :exclusions [org.clojure/tools.nrepl]}}
+                                  cider.nrepl/wrap-version]}}}

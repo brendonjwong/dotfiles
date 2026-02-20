65a3efb07f2f37911d296aeeba89dca77f42b3ab
brendonjwong <brendon@amperity.com>
2026-02-20 14:26:07 -0800

    Replace spectacle with rectangle
    The latter has been discontinued.

--- spectacle/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Symlink spectacle settings
-
-CURRENT_DIR="$(dirname "$0")"
-SETTINGS_DIR="$HOME/Library/Application Support/Spectacle"
-
-ln -fs "$CURRENT_DIR"/Shortcuts.json "$SETTINGS_DIR"/Shortcuts.json

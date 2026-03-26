1f0f40cb3605e14d494f6eba1a894711b19b7783
brendonjwong <bjwong.w@gmail.com>
2026-03-26 00:58:23 -0700

    Fix command to setup nvim config

--- neovim/install.sh
+++ neovim/install.sh
@@ -3,7 +3,7 @@
 # Install settings for neovim
 
 # Symlink nvim config
-ln -s $(dirname "$0")/.config/nvim ~/.config/
+ln -s $(git rev-parse --show-toplevel)/.config/nvim ~/.config/
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
